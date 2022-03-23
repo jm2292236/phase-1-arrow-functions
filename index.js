00f416c1a3be5630368f08c7b1592c058f7a9acb
Jaime Montes de Oca <jaime0422@live.com>
2022-03-22 22:51:57 -0700

    Completed assignment

---
+++ index.js
@@ -0,0 +1,10 @@
+// Function expression
+const divide = function () {return 2000 / 100};
+
+// Arrow function with only one parameter
+// No parenthesis needed to enclose the parameter
+const square = number => number * number;
+
+// Arrow function with more than one paramenter
+// Parenthesis and comma are necessary for the parameters
+const add = (number1, number2) => number1 + number2;
